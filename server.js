c316642744defd6009273bbe9738c7634f79b5dc
rich kevan <rskevan@me.com>
2021-11-18 16:18:13 -0800

    added post route

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+const shortid = require('shortid');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -14,10 +16,17 @@ app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.post('/api/shorturl', function(req, res) {
+  console.log(req.body);
+  res.json({orginal_url: req.body.url, short_url: shortid.generate() });
+})
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
