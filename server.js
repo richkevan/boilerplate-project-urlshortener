ff7370cc9b63e44007a27228e72fbe608bd1e193
rich kevan <rskevan@me.com>
2021-12-03 18:42:00 -0800

    shortend url service

--- server.js
+++ server.js
@@ -12,9 +12,9 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
-
 app.locals.basedir = __dirname;
+
+app.use('/public', express.static(__dirname + `/public`));
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
@@ -27,22 +27,16 @@ app.use(express.urlencoded({ extended: true }));
 
 let urls = new Object();
 
-
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
 app.post('/api/shorturl', function(req, res) {
     const httpRegex = /^(http|https)(:\/\/)/;
     if (!httpRegex.test(req.body.url)) {return res.json({ error: 'invalid url' })}
     const {href, host, pathname, protocol } = new URL(req.body.url);
     let short = shortid.generate();
     urls[`${short}`] = [href,short]
-    res.render('short',{original_url : href, short_url : short , base: req.get('host')+'/api/shorturl/'})
+    res.render('short',{original_url : href, short_url : short , base: req.get('host')})
   });
 
-app.get('/api/shorturl/:short_url', function (req, res) {
+app.get('/:short_url', function (req, res) {
   res.redirect(urls[req.params.short_url][0]);
 })
 
