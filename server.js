ade0be649ce5ec963a22b627b78b334e88de5e7c
rich kevan <rskevan@me.com>
2021-12-03 17:36:22 -0800

    Updated to pug template and ico

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
 const app = express();
 const shortid = require('shortid');
 const url = require('url');
+const pug = require('pug');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -12,13 +14,19 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
+app.locals.basedir = __dirname;
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
 app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.render('index');
 });
 
 app.use(express.urlencoded({ extended: true }));
 
 let urls = new Object();
+
 
 // Your first API endpoint
 app.get('/api/hello', function(req, res) {
@@ -31,7 +39,7 @@ app.post('/api/shorturl', function(req, res) {
     const {href, host, pathname, protocol } = new URL(req.body.url);
     let short = shortid.generate();
     urls[`${short}`] = [href,short]
-    res.json({original_url : href, short_url : short });
+    res.render('short',{original_url : href, short_url : short , base: __dirname});
   });
 
 app.get('/api/shorturl/:short_url', function (req, res) {
